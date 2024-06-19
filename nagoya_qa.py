8ca4bc85bfbdea6c296a6aa0ecef01bfec296093
Onur Boyar <boyaronur@gmail.com>
2024-06-19 11:26:14 +0900

    ds

--- nagoya_qa.py
+++ nagoya_qa.py
@@ -15,7 +15,7 @@ train_args = {
     'overwrite_output_dir': True,
     "evaluate_during_training": True,
     "max_seq_length": 128,
-    "num_train_epochs": 25, #25, after experimentations
+    "num_train_epochs": 50, #25, after experimentations
     "evaluate_during_training_steps": 500,
     "save_model_every_epoch": False,
     "save_eval_checkpoints": False,
